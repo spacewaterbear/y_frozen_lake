8d9449e2dc1c7875fb5fb425e89c95c7e1c723ad
Emmanuel Thiery <emmanuel.thiery@protonmail.com>
2023-01-12 17:24:06 +0100

    monte carlo + eval

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import gym
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
-
+from loguru import logger
 from models.rl_params import Params
+from rl_agent.q_monte_carlo_agent import QMonteCarloAgent
 from rl_agent.q_td_agent import QTDAgent
 
 
@@ -20,19 +21,37 @@ def plot_V(V):
     plt.show()
 
 
+def eval(env: gym.Env, agent, nb_eval_episode: int=50):
+    rewards = []
+    for _ in range(nb_eval_episode):
+        done = False
+        state = env.reset()
+        while not done:
+            action = agent.policy(state, greedy=True)
+            next_state, reward, done, info = env.step(action)
+            state = next_state
+        rewards.append(reward)
+    mean_rewards = np.mean(rewards)
+    std_rewards = np.std(rewards)
+    return mean_rewards, std_rewards
+
+
+
 if __name__ == '__main__':
 
     env = gym.make('FrozenLake-v1', desc=None, map_name="4x4", is_slippery=False)
     params = Params(epsilon=0.1)
     nb_episodes = 1000
-    agent = QTDAgent(env=env, params=params)
+    agent = QMonteCarloAgent(env=env, params=params)
     for _ in range(nb_episodes):
         done = False
         state = env.reset()
         while not done:
             action = agent.policy(state)
             next_state, reward, done, info = env.step(action)
-            agent.update_value_function(state, reward, next_state, action=action)
+            agent.add_history(state=state, reward=reward, action=action) # store reward, state, action
             state = next_state
+        agent.update_q_value()
     agent.plot()
-    # plot_V(agent.Q)
+    mean_r, std_r = eval(env, agent)
+    logger.info(f"{mean_r=}, {std_r}")
---
+++ rl_agent/q_monte_carlo_agent.py
@@ -0,0 +1,39 @@
+import random
+
+import gym
+import numpy as np
+
+from models.rl_params import Params
+
+
+np.random.seed(0)
+
+class QMonteCarloAgent:
+    def __init__(self, env: gym.Env, params: Params):
+        self.env = env
+        self.Q = np.zeros((env.observation_space.n, env.action_space.n))
+        self.params = params
+        self.history = []
+    def policy(self, state, greedy=False):
+        """greedy action always"""
+        if np.random.random() < self.params.epsilon and not greedy:
+            return self.env.action_space.sample()
+        else:
+            return np.random.choice(np.flatnonzero(self.Q[state] == self.Q[state].max()))
+            # return np.argmax(self.Q[state])
+
+    def plot(self):
+        V = np.max(self.Q, axis=1)
+        print(V)
+
+    def add_history(self, state, reward, action):
+        self.history.append((state, reward, action))
+
+    def update_q_value(self):
+        G = 0
+        for state, reward, action in reversed(self.history):
+            G = self.params.gamma*G+reward
+            self.Q[state, action] = self.Q[state, action] + self.params.alpha*(G-self.Q[state, action])
+        self.history = []
+
+
